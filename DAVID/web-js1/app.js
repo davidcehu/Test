b2fb042e31ae5d0d1be845275467d5cc66aef4e3
David <200060@iessierradeguara.com>
2020-11-06 11:42:10 +0100

    web js1

---
+++ DAVID/web-js1/app.js
@@ -0,0 +1,32 @@
+// alert("Hola mundo");
+
+//Comentario de una línea
+
+/*
+Esto es 
+un comentario multilínea
+*/
+
+/* Esto es un comentario*/
+
+// Tipos de datos
+
+// Tipo cadena de texto o string
+// alert("Esto es una cadena de texto");
+// alert('Esto también es una cadena de texto');
+// alert("I'm the \"teacher\" ");
+// alert('I\'m the teacher');
+
+// Tipo número entero
+// se pueden usar operadores aritméticos
+// alert("2" + 3);
+// alert(2 - 3);
+// alert(2 * 3);
+// alert(2 / 3);
+
+// Tipo número decimal
+// se pueden usar operadores aritméticos
+alert(2.5 + 3.1);
+alert(2.7 - 3.5);
+alert(21.1 * 3.2);
+alert(2.87 / 3.5);
