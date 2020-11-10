d643b63796203088425fa5db44c02abc8608949f
David <200060@iessierradeguara.com>
2020-11-10 19:37:33 +0100

    fin clase 10nov

---
+++ DAVID/web-js1/app.js
@@ -0,0 +1,13 @@
+let resultado;
+
+resultado = confirm("¿Quieres que te salude?");
+
+if (resultado == true) {
+    alert("Hola");
+}else{
+    alert("Eres un desagradecido!");
+}
+alert("fin del progama");
+
+
+
